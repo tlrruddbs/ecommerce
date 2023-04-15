1aa06219562d0a408b74f87b7f0402a408e407a6
kyungsik <tlrruddbs@naver.com>
2023-04-16 04:24:38 +0900

    eureka-4: jwt 1차 구현

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -2,10 +2,20 @@ package com.example.userservice.service;
 
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
     Iterable<UserEntity> getUserByAll();
+
+    UserDto getUserDetailsByEmail(String userName);
+
+    @Override
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+
 }
