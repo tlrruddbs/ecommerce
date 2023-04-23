647c1a1daca3931001be853497d3dbf78521de2a
kyungsik <tlrruddbs@naver.com>
2023-04-24 01:59:39 +0900

    eureka-5: 임시저장

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,13 +1,21 @@
 package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public HttpExchangeRepository httpTraceRepository(){
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
